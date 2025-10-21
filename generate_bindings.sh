9556282bebf0e130a42acf11f837c088f95756fe
Armand <4831c0@proton.me>
2025-10-21 19:36:27 +0200

    feat: add support for dart
    Currently the uniffi-dart package isn't
    available on crates.io and when publishing
    to crates.io you aren't allowed to
    have any git dependencies, so I wrote a
    helper script and added a diff

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+git apply dart.diff
 cargo build --release \
   && cargo run --bin uniffi-bindgen generate --library target/release/libcep.so \
     --language kotlin \
     --language python \
     --out-dir out \
   && uniffi-bindgen-go src/cep.udl -o out
+git apply dart.diff -R
+git checkout Cargo.lock
